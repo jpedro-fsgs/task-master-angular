cbc866eb0dcb23ff450d22a11f76d75c98217fd4
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2024-12-28 09:12:48 -0300

    feat: tic tac toe

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tic-tac-toe',
-  imports: [],
   templateUrl: './tic-tac-toe.component.html',
-  styleUrl: './tic-tac-toe.component.scss'
+  styleUrls: ['./tic-tac-toe.component.scss'],
+  imports: [CommonModule]
 })
 export class TicTacToeComponent {
+  board: string[][] = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+  currentPlayer: string = 'X';
+  winner: string | null = null;
 
+  makeMove(row: number, col: number): void {
+    if (!this.board[row][col] && !this.winner) {
+      this.board[row][col] = this.currentPlayer;
+      if (this.checkWinner()) {
+        this.winner = this.currentPlayer;
+      } else {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    }
+  }
+
+  checkWinner(): boolean {
+    const lines = [
+      // Rows
+      [this.board[0][0], this.board[0][1], this.board[0][2]],
+      [this.board[1][0], this.board[1][1], this.board[1][2]],
+      [this.board[2][0], this.board[2][1], this.board[2][2]],
+      // Columns
+      [this.board[0][0], this.board[1][0], this.board[2][0]],
+      [this.board[0][1], this.board[1][1], this.board[2][1]],
+      [this.board[0][2], this.board[1][2], this.board[2][2]],
+      // Diagonals
+      [this.board[0][0], this.board[1][1], this.board[2][2]],
+      [this.board[0][2], this.board[1][1], this.board[2][0]]
+    ];
+
+    return lines.some(line => line.every(cell => cell === this.currentPlayer));
+  }
+
+  resetGame(): void {
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ];
+    this.currentPlayer = 'X';
+    this.winner = null;
+  }
 }
