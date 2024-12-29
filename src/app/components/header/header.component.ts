00383b95f3d441de34c0168dae27f230d1a4bab4
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2024-12-29 15:30:48 -0300

    fix: responsivity improved

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,13 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
-  // title = signal('Hello Angular!');
-  title = input('Hello Angular!');
-
+  title = input();
 }
